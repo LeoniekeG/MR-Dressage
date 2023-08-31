7793b9c12a47ec1da6f262dd9e1c9e077d212440
Leonieke Gootjes <info@dalmatianstuddogs.com>
2023-08-31 11:30:45 +0200

    Update

--- src/pages/List.js
+++ src/pages/List.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export default function List ({ horses, title }) {
+export default function List ({ horses, title }) { 
 
     return (
         <div className="list">
