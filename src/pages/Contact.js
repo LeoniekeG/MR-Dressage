70360bd1c55c78c944a5069cd224c715dbb93b99
Leonieke Gootjes <info@dalmatianstuddogs.com>
2023-09-28 11:26:38 +0200

    Under construction

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,6 +4,7 @@ export default function Contact () {
     return (
         <div className="contact">
             <h1>Contact gegevens</h1>
+            <h3>Under construction</h3>
         </div>
     )
 }
